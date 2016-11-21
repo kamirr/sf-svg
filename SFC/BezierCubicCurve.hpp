12a2f0f0776772c1b9e9181a26639b9a486f2e95
koczurekk <koczurekk@gmail.com>
2016-11-21 22:30:45 +0100

    [Docs] BezierCubicCurve

--- SFC/BezierCubicCurve.hpp
+++ SFC/BezierCubicCurve.hpp
@@ -2,12 +2,20 @@
  * SF-Curves (c) Kamil Koczurek | koczurekk@gmail.com          *
  * GNU GPL v3 License http://www.gnu.org/licenses/gpl-3.0.html *
  * =========================================================== */
+//! @file
+//! BezierCubicCurve declaration
 
 #ifndef BEZIERCUBICCURVE_HPP
 #define BEZIERCUBICCURVE_HPP
 #include "BezierAbstractCurve.hpp"
 
 namespace sfc {
+	//! \class BezierCubicCurve
+	//!
+	//! \brief Bezier cubic curve representation
+	//!
+	//! Can be drawn, inherits BezierAbstractCurve. <br />
+	//! Use to draw/calculate more complex curves, like entities paths.
 	class BezierCubicCurve
 	: public BezierAbstractCurve {
 	private:
@@ -19,9 +27,21 @@ namespace sfc {
 		virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
 
 	protected:
+		//! \brief Calculate points on cuve
+		//!
+		//! \param [out] vector – vector to store vertices
+		//! \param [in]  numSegments – aka point count
+		//! \param [in]  limitCurve – part of curve to be drawn, range [0, 1]
+		//! \param [out]  debugLines – vertex array, used to store and draw helper lines
+		//!
+		//! Calculates all (cubic) curve-related things like vertices positions / helper lines etc.
+		//! Not a big deal, use only if you really want to inherit BezierCubicCurve.
 		void calculateCubic(std::vector<sf::Vector2f>&, const size_t, const float, sf::Vertex*);
 
 	public:
+		//! \brief Point type enum
+		//!
+		//! Used to easly set/get points with one function –setPointPosition / getPointPosition.
 		enum PointType {
 			BEGIN_CONTROL,
 			END_CONTROL,
@@ -29,20 +49,52 @@ namespace sfc {
 			END
 		};
 
-		/* Constructors */
+		//! \brief Default constructor
+		//!
+		//! Sets point count to 32 and color to white.
 		BezierCubicCurve();
+
+		//! \brief Specialized constructor
+		//!
+		//! \param [in] start – beginning of the curve
+		//! \param [in] end – end of the curve
+		//! \param [in] startControl – first control point
+		//! \param [in] endContril – second control point
+		//!
+		//! Sets point count to 32 and color to white.
+		//! Calls calculateCubic with proper args.
 		BezierCubicCurve(const sf::Vector2f&, const sf::Vector2f&, const sf::Vector2f&, const sf::Vector2f&, DrawMode = NORMAL);
 
-		/* Virtual methods */
+		/* Commented in base class */
 		virtual Line2<float> getHelperLine(const float) const;
+
+		/* Commented in base class */
 		virtual sf::Vector2f getPoint(std::size_t) const;
+
+		/* Commented in base class */
 		virtual void update();
 
-		/* Class-definied API */
+		//! \brief Sets position of selected point
+		//!
+		//! \param [in] point – point type (PointType enum)
+		//! \param [in] pos – new position
 		void setPointPosition(PointType, sf::Vector2f);
+
+		//! \brief Gets position of selected point
+		//!
+		//! \param [in] point – point type (PointType enum)
+		//!
+		//! \returns sf::Vector2f (position)
 		sf::Vector2f getPointPosition(PointType);
 
+		//! \brief Gets 1st helper line used to draw curve
+		//!
+		//! \param [in] normalized – Normalized ([0, 1]) progress
 		Line2<float> getCubicHelperLine1(const float);
+
+		//! \brief Gets 2nd helper line used to draw curve
+		//!
+		//! \param [in] normalized – Normalized ([0, 1]) progress
 		Line2<float> getCubicHelperLine2(const float);
 	};
 }
