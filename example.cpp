30fae415fb8238b70b2a682e270b561e1ddd2dbc
koczurekk <koczurekk@gmail.com>
2016-12-09 10:48:27 +0100

    Updated image

--- example.cpp
+++ example.cpp
@@ -7,21 +7,14 @@ int main() {
 	/* Create image */
 	sfc::SVGImage img;
 
-	/* Load svg file */
-	img.loadFromFile("nano.svg");
+	/* Load SVG image from file */
+	img.loadFromFile("media/car.svg");
 
 	/* Move it by [10, 10] to make it more visible */
 	img.move({10, 10});
 
-	/* Scale image */
-	const auto imageScale = 2.5f;
-	img.scale(imageScale);
-
-	/* Rasterize using scale 2.f and save result (sf::Image) to file */
-	img.rasterize(2.f).saveToFile("rasterized.png");
-
 	/* Create window */
-	sf::RenderWindow app(sf::VideoMode((img.getSize().x + 20) * imageScale, (img.getSize().y + 20) * imageScale), "sf-svg");
+	sf::RenderWindow app(sf::VideoMode((img.getSize().x + 20) * 1, (img.getSize().y + 20) * 1), "sf-svg");
 
 	auto i = 0u;
 	while(app.isOpen()) {
