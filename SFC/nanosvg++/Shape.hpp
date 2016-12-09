cda3096bcd84d87860b306a3c75ba5d2e6dd90e6
koczurekk <koczurekk@gmail.com>
2016-12-09 21:01:35 +0100

    Removed extra comment

--- SFC/nanosvg++/Shape.hpp
+++ SFC/nanosvg++/Shape.hpp
@@ -2,30 +2,7 @@
 #define SHAPE_HPP
 
 #include <string>
-
 #include "nanosvg.hpp"
-
-/*
-	struct ShapeStruct
-	{
-		char id[64];				// Optional 'id' attr of the shape or its group
-		Paint fill;				// Fill paint
-		Paint stroke;			// Stroke paint
-		float opacity;				// Opacity of the shape.
-		float strokeWidth;			// Stroke width (scaled).
-		float strokeDashOffset;		// Stroke dash offset (scaled).
-		float strokeDashArray[8];			// Stroke dash array (scaled).
-		char strokeDashCount;				// Number of dash values in dash array.
-		LineJoin strokeLineJoin;		// Stroke join type.
-		LineCap strokeLineCap;			// Stroke cap type.
-		FillRule fillRule;				// Fill rule, see NSVGfillRule.
-		Flags flags;		// Logical or of NSVG_FLAGS_* flags
-		float bounds[4];			// Tight bounding box of the shape [minx,miny,maxx,maxy].
-
-		Path* paths;			// Linked list of paths in the image.
-		ShapeStruct* next;		// Pointer to next shape, or NULL if last element.
-	};
-*/
 
 namespace nsvg {
 	class Shape {
