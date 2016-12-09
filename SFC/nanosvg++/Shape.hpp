255c16f1f7dadacf0e847080b84713286bd0e108
koczurekk <koczurekk@gmail.com>
2016-12-09 21:14:51 +0100

    Documented nsvg::Shape class

--- SFC/nanosvg++/Shape.hpp
+++ SFC/nanosvg++/Shape.hpp
@@ -16,40 +16,93 @@
 #include "nanosvg.hpp"
 
 namespace nsvg {
+	//! \class Shape
+	//!
+	//! \brief C++-styled cstyle::ShapeStruct wrapper
 	class Shape {
 		const cstyle::ShapeStruct* internal;
 
 	public:
+		//! \brief Constructor
+		//!
+		//! \param [in] ptr – const pointer to ShapeStruct
+		//!
+		//! It's the only constructor of nsvg::Shape class, which wraps given cstyle::ShapeStruct into object orientated interface.
 		Shape(const cstyle::ShapeStruct *ptr);
 
+		//! \brief Get ID
+		//!
+		//! \return Shape's ID
 		std::string getId() const;
 
+		//! \brief Get fill paint
+		//!
+		//! \return cstyle::Paint structure
 		cstyle::Paint getFill() const;
 
+		//! \brief Get stroke
+		//!
+		//! \return Stroke
 		cstyle::Paint getStroke() const;
 
+		//! \brief Get opacity
+		//!
+		//! \return Floating-point opacity
 		float getOpacity() const;
 
+		//! \brief Get stroke width
+		//!
+		//! \return Stroke width
 		float getStrokeWidth() const;
 
+		//! \brief Get stroke dash offset
+		//!
+		//! \return Stroke dash offset
 		float getStrokeDashOffset() const;
 
+		//! \brief Get stroke dash array
+		//!
+		//! \return Const float pointer to stroke dash array
 		const float* getStrokeDashArray() const;
 
+		//! \brief Get stroke dash count
+		//!
+		//! \return Unsigned stroke dash count
 		size_t getStrokeDashCount() const;
 
+		//! \brief Get stroke line join
+		//!
+		//! \return LineJoin enum
 		LineJoin getStrokeLineJoin() const;
 
+		//! \brief Get stroke line cap
+		//!
+		//! \return LineCap enum
 		LineCap getStrokeLineCap() const;
 
+		//! \brief get Flags
+		//!
+		//! \return Flags enum
 		Flags getFlags() const;
 
+		//! \brief Get bounds
+		//!
+		//! \return Const float pointer to bounds array
 		const float* getBounds() const;
 
+		//! \brief Get paths
+		//!
+		//! \return const cstyle::Path pointer to path
 		const cstyle::Path* getPaths() const;
 
+		//! \brief Get next shape
+		//!
+		//! \return Instance of shape wrapping this->internal->next.
 		Shape getNextShape() const;
 
+		//! \brief Is ok?
+		//!
+		//! \return True if instance wraps proper pointer (not nullptr), False otherwise.
 		bool good() const {
 			return internal;
 		}
